6e758beab8371d8338be23c4d1aedc5873861325
Loan ALOUACHE <alouache.loan@gmail.com>
2021-07-21 11:39:23 +0200

    Added Swagger doc integration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
@@ -6,6 +7,17 @@ import config from './assets/config.json';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+	// Configure swagger
+	const swaggerConfig = new DocumentBuilder()
+		.setTitle('Swagger starter')
+		.setDescription('NestJS web-server starter kit API')
+		.setVersion('1.0')
+		.addTag('users')
+		.build();
+	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('api', app, swaggerDocument);
+
 	await app.listen(config.server.port);
 }
 bootstrap();
