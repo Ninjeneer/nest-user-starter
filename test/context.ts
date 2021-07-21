167a55160142baff85c7ccd22e9aade4858cc37b
Loan ALOUACHE <alouache.loan@gmail.com>
2021-07-21 09:58:58 +0200

    Added test context generator

---
+++ test/context.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+import { SecurityService } from '../src/security/security.service';
+import { UserRole } from '../src/user/user.service';
+import config from '../src/assets/config.json';
+
+const prisma = new PrismaClient();
+const securityService = new SecurityService();
+
+prisma.user
+	.create({
+		data: {
+			email: config.tests.admin.email,
+			password: securityService.hashPassword(config.tests.admin.password),
+			role: UserRole.ADMIN
+		}
+	})
+	.catch((e) => console.log(e));
+
+prisma.user
+	.create({
+		data: {
+			email: config.tests.basic.email,
+			password: securityService.hashPassword(config.tests.basic.password),
+			role: UserRole.BASIC
+		}
+	})
+	.catch((e) => console.log(e));
